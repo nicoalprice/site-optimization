7ac7658474c8aef2c0ee3ab2ca023292ea66963e
Nicoal Price <nicoalprice@gmail.com>
2017-01-12 16:29:47 -0500

    more pseudocode for modal images

--- js/modal-images.js
+++ js/modal-images.js
@@ -3,12 +3,14 @@
 $('img').click( function() {
 	console.log('You clicked an image.');
 	// check src
-	//change src for modal img -small
+	var source = $this.attr('src');
+	var photoID = $(this.attr('photo-reveal-id');
+
+	//change src for modal img -small or add large
 
 
 	// get photo-reveal-id
 });
 
-//var photoID = $(this.attr('photo-reveal-id');
 //var photoNumber = photoID [minus the first portion, photo1-]
 //$('#photoID').attr('src', [image]);
